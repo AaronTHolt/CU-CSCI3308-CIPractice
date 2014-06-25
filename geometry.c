a32356b508adf7474cb05dca2e8d3c90bfbb2e9f
Aaron Holt <holtat@csel-112-02.csel.loc>
2014-06-25 19:15:10 +0000

    jdfadjklf

--- geometry.c
+++ geometry.c
@@ -69,3 +69,12 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
+	return abs(((a->x)*(b->y-c->y)+(b->x)*(c->y-a->y)+c->x*(a->y-b->y))/2);
+}
+
+bool approximatelyEqual(float a, float b, float epsilon)
+{
+    return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
